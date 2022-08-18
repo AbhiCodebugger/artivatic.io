9b04f9b3ad1f474d7d28655f5540d0f24708b100
Abhinav Ranjan <ranjanTC>
2022-08-18 14:38:31 +0530

    addedpull to refresh

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,8 +2,20 @@ import 'package:artivatic_assignment/controller/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
+    setState(() {
+      debugPrint('refresh');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,64 +34,74 @@ class HomeScreen extends StatelessWidget {
                       ? const Center(
                           child: CircularProgressIndicator(),
                         )
-                      : ListView.builder(
-                          itemCount: controller.list.rows!.length,
-                          itemBuilder: ((context, index) {
-                            return controller.isLoading
-                                ? const Center(
-                                    child: CircularProgressIndicator(),
-                                  )
-                                : Card(
-                                    elevation: 4.0,
-                                    margin: const EdgeInsets.all(6),
-                                    child: Row(
-                                      children: [
-                                        Flexible(
-                                            flex: 1,
-                                            child: controller.list.rows![index]
-                                                        .imageHref ==
-                                                    null
-                                                ? Image.asset(
-                                                    'assets/placeholder.png')
-                                                : Image.network(
-                                                    controller.list.rows![index]
-                                                        .imageHref!,
-                                                    fit: BoxFit.cover,
-                                                    errorBuilder: (context,
-                                                        error, stackTrace) {
-                                                      return Image.asset(
-                                                          'assets/placeholder.png');
-                                                    },
-                                                  )),
-                                        const SizedBox(width: 6),
-                                        Flexible(
-                                            flex: 2,
-                                            fit: FlexFit.loose,
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  controller.list.rows![index]
-                                                          .title ??
-                                                      "-",
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .titleMedium!
-                                                      .copyWith(
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                ),
-                                                Text(controller
-                                                        .list
-                                                        .rows![index]
-                                                        .description ??
-                                                    "-"),
-                                              ],
-                                            ))
-                                      ],
-                                    ));
-                          })));
+                      : RefreshIndicator(
+                          onRefresh: onRefresh,
+                          child: ListView.builder(
+                              itemCount: controller.list.rows!.length,
+                              itemBuilder: ((context, index) {
+                                return controller.isLoading
+                                    ? const Center(
+                                        child: CircularProgressIndicator(),
+                                      )
+                                    : Card(
+                                        elevation: 4.0,
+                                        margin: const EdgeInsets.all(6),
+                                        child: Row(
+                                          children: [
+                                            Flexible(
+                                                flex: 1,
+                                                child: controller
+                                                            .list
+                                                            .rows![index]
+                                                            .imageHref ==
+                                                        null
+                                                    ? Image.asset(
+                                                        'assets/placeholder.png')
+                                                    : Image.network(
+                                                        controller
+                                                            .list
+                                                            .rows![index]
+                                                            .imageHref!,
+                                                        fit: BoxFit.cover,
+                                                        errorBuilder: (context,
+                                                            error, stackTrace) {
+                                                          return Image.asset(
+                                                              'assets/placeholder.png');
+                                                        },
+                                                      )),
+                                            const SizedBox(width: 6),
+                                            Flexible(
+                                                flex: 2,
+                                                fit: FlexFit.loose,
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      controller
+                                                              .list
+                                                              .rows![index]
+                                                              .title ??
+                                                          "-",
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .titleMedium!
+                                                          .copyWith(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold),
+                                                    ),
+                                                    Text(controller
+                                                            .list
+                                                            .rows![index]
+                                                            .description ??
+                                                        "-"),
+                                                  ],
+                                                ))
+                                          ],
+                                        ));
+                              })),
+                        ));
         });
   }
 }
